8d1f415b34db98fe4155257ae232a8ea67956ef9
Shubhankar Warkade <140995557+Shubhankar759@users.noreply.github.com>
2026-06-15 23:38:18 +0530

    Add: set-mismatch - submission-3

---
+++ Data Structures & Algorithms/set-mismatch/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        res =[0,1]
+        for i in range(1,len(nums)):
+            if nums[i-1] == nums[i]:
+                res[0]=nums[i]
+            elif nums[i] - nums[i - 1] == 2:
+                res[1] = nums[i] - 1
+
+
+        if nums[-1] != len(nums):
+            res[1] = len(nums)
+        return res
+        
